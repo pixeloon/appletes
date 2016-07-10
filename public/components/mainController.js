8e0d667f1b411fa2d303b14c3da48b1a8b1abbd9
Gunther Schneider <pixeloon@gmail.com>
2016-07-10 10:06:51 -0700

    interface changes for tagging

--- public/components/mainController.js
+++ public/components/mainController.js
@@ -3,7 +3,8 @@
     "use strict";
 
     angular.module("appletesApp")
-        .controller("MainController", function($scope, $http, WorkoutFactory, ExerciseFactory, $element) {
+        .controller("MainController", function($scope, $http, $element, $mdSidenav, $timeout, $q, WorkoutFactory, ExerciseFactory) {
+
             WorkoutFactory.getWorkouts().then(function(workouts) {
                 $scope.view.cards = workouts.data;
             });
@@ -11,27 +12,12 @@
             ExerciseFactory.getExercises().then(function(exercises) {
                 $scope.view.exercises = exercises.data;
             });
-
+            window.scope = $scope
             $scope.view = {};
             $scope.view.exerciseCounter = 4;
             $scope.view.workoutCounter = 0;
             $scope.view.cardCounter = 0;
             $scope.view.showCards = true;
-            $scope.view.showNewWorkoutForm = false;
-            // $scope.view.exerciseList = [];
-
-
-            $scope.view.tags = ['strength', 'endurance', 'flexibility', 'arms', 'legs', 'chest'];
-            $scope.searchTerm;
-            $scope.clearSearchTerm = function() {
-                $scope.searchTerm = '';
-            };
-            // disabling md-select directive's quick select functionality since we have a search input 
-            $element.find('input').on('keydown', function(ev) {
-                ev.stopPropagation();
-            });
-
-           
 
             $scope.submitWorkout = function() {
                 $scope.view.workouts.push({
@@ -46,7 +32,80 @@
                 });
             }
 
+            $scope.openRightNav = function() {
+                $mdSidenav('right').open();
+            };
 
-        });
+            $scope.closeRightNav = function() {
+                $mdSidenav('right').close();
+            };
+
+        })
+
+    .controller('TagController', TagCtrl);
+
+    function TagCtrl($timeout, $q) {
+        var self = this;
+        self.readonly = false;
+        self.selectedItem = null;
+        self.searchText = null;
+        self.querySearch = querySearch;
+        self.tags = loadTags();
+        self.selectedTags = [];
+        self.numberChips = [];
+        self.numberChips2 = [];
+        self.numberBuffer = '';
+        self.autocompleteRequireMatch = true;
+        self.transformChip = transformChip;
+        /**
+         * Return the proper object when the append is called.
+         */
+        function transformChip(chip) {
+            // If it is an object, it's already a known chip
+            if (angular.isObject(chip)) {
+                return chip;
+            }
+            // Otherwise, create a new one
+            return { name: chip }
+        }
+        /**
+         * Search for tags.
+         */
+        function querySearch(query) {
+            var results = query ? self.tags.filter(createFilterFor(query)) : [];
+            return results;
+        }
+        /**
+         * Create filter function for a query string
+         */
+        function createFilterFor(query) {
+            var lowercaseQuery = angular.lowercase(query);
+            return function filterFn(tag) {
+                return (tag._lowername.indexOf(lowercaseQuery) === 0);
+            };
+        }
+
+        function loadTags() {
+            var tags = [{
+                'name': 'endurance'
+            }, {
+                'name': 'strength'
+            }, {
+                'name': 'flexibility'
+            }, {
+                'name': 'arms'
+            }, {
+                'name': 'legs'
+            }];
+            return tags.map(function(tag) {
+                tag._lowername = tag.name.toLowerCase();
+                return tag;
+            });
+        }
+    }
+
+
+
+
 
 })();
